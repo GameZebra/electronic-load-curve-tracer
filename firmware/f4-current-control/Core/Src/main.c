ef8caba11656f1332988614010bdb7ab4784a59e
GameZebra <mr.xmitkotox@gmail.com>
2026-06-25 19:06:45 +0300

    tests with known good Discovery board

--- firmware/f4-current-control/Core/Src/main.c
+++ firmware/f4-current-control/Core/Src/main.c
@@ -344,7 +344,7 @@ static void MX_TIM8_Init(void)
   htim8.Instance = TIM8;
   htim8.Init.Prescaler = 48-1;
   htim8.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim8.Init.Period = 20000;
+  htim8.Init.Period = 1000;
   htim8.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim8.Init.RepetitionCounter = 0;
   htim8.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
